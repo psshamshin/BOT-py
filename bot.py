6787981ee0796b41f77b675caf2281913a4ea511
psshamshin <57655945+psshamshin@users.noreply.github.com>
2021-02-09 23:23:37 +0300

    release v1.1

--- bot.py
+++ bot.py
@@ -22,53 +22,58 @@ def welcome(message):
     # markup keyboard
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    but_1 = types.KeyboardButton("☂")
+    but_1 = types.KeyboardButton("✉")
     but_2 = types.KeyboardButton("⏳")
     but_3 = types.KeyboardButton("❔")
+    but_4 = types.KeyboardButton("✒")
+    but_5 = types.KeyboardButton("✏")
 
-    markup.add(but_1, but_2, but_3)
+    markup.add(but_1, but_2, but_3, but_4, but_5)
 
     bot.send_message(message.chat.id, "Общий салам, {0.first_name} \nВремени с UNIX: {1} секунд"
-                     .format(message.from_user, message.date),
+                     .format(message.from_user, message.date + 10800),
                      parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def chat(message):
-    current_time = datetime.fromtimestamp(message.date).strftime('%H:%M:%S')
+    current_date_of_week = datetime.fromtimestamp(message.date + 10800).today().weekday()
+    current_time = datetime.fromtimestamp(message.date + 10800).strftime('%H:%M:%S')
     if message.chat.type == 'private':
         if message.text == '⏳':
             bot.send_message(message.chat.id, "Дата: {0}\nДень недели: {1}"
-                             .format(datetime.fromtimestamp(message.date).strftime('%d.%m.%Y\n%H:%M:%S'),
-                                     day_of_week[datetime.fromtimestamp(message.date).today().weekday()]))
+                             .format(datetime.fromtimestamp(message.date + 10800).strftime('%d.%m.%Y\n%H:%M:%S'),
+                                     day_of_week[datetime.fromtimestamp(message.date + 10800).today().weekday()]))
             # subjects table
             flag = True
-            for i in range(len(timetable[day_of_week[datetime.fromtimestamp(message.date).today().weekday()]])):
-                if datetime.fromtimestamp(message.date).today().weekday() != 5 or 6:
+            for i in range(len(timetable[day_of_week[datetime.fromtimestamp(message.date + 10800).today().weekday()]])):
+                if current_date_of_week == 5 or current_date_of_week == 6:
                     bot.send_message(message.chat.id, 'чил')
                     break
                 if current_time > start[i]:
                     if current_time < end[i]:
                         # lesson is going
                         bot.send_message(message.chat.id, '✅' +
-                                         timetable[day_of_week[datetime.fromtimestamp(message.date).today().weekday()]]
-                                         [i] + ' - ' + str(gap(current_time, end[i])) + ' до конца')
+                                         timetable[day_of_week[datetime.fromtimestamp(message.date + 10800).today().
+                                         weekday()]][i] + ' - ' + str(gap(current_time, end[i])) + ' до конца')
                         flag = False
                     else:
                         # end of lesson
                         bot.send_message(message.chat.id, '❌' +
-                                         timetable[day_of_week[datetime.fromtimestamp(message.date).today().weekday()]]
-                                         [i])
+                                         timetable[day_of_week[datetime.fromtimestamp(message.date + 10800).today().
+                                         weekday()]][i])
                         flag = True
                 else:
                     if flag:
                         bot.send_message(message.chat.id, '✓' +
-                                         timetable[day_of_week[datetime.fromtimestamp(message.date).today().weekday()]]
+                                         timetable[day_of_week[
+                                             datetime.fromtimestamp(message.date + 10800).today().weekday()]]
                                          [i] + ' - ' + str(gap(current_time, start[i])) + ' до начала')
                         flag = False
                     else:
                         bot.send_message(message.chat.id, '✓' +
-                                         timetable[day_of_week[datetime.fromtimestamp(message.date).today().weekday()]]
+                                         timetable[day_of_week[
+                                             datetime.fromtimestamp(message.date + 10800).today().weekday()]]
                                          [i])
         elif message.text == '❔':
             markup2 = types.InlineKeyboardMarkup(row_width=2)
@@ -77,8 +82,29 @@ def chat(message):
             markup2.add(item1, item2)
 
             bot.send_message(message.chat.id, 'Пойди нахуй?', reply_markup=markup2)
+        elif message.text == '✒':
+            bot.send_animation(chat_id=message.chat.id, animation='https://i.gifer.com/O9Yw.gif')
+        elif message.text == '✏':
+            bot.send_message(message.chat.id, 'Команды:\nrandom - рандомное значение кубика')
+        elif message.text == 'voice':
+            bot.send_voice(chat_id=message.chat.id,
+                           voice='https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+                           caption="ЛОЛ")
+        elif message.text == 'random':
+            bot.send_dice(chat_id=message.chat.id, emoji='🎲')
+        elif message.text == '✉':
+            bot.send_photo(chat_id=message.chat.id, photo='https://sun9-47.userapi.com/impg/b_EUaMN45'
+                                                          '-EtPfd6WpaVGw1_kszLS5j8SQeMZg/xq6vTxs9dhk.jpg?size'
+                                                          '=1218x1624&quality=96&proxy=1&sign'
+                                                          '=4ad3ec8afbbacbfcd75253b9e3777c50&type=album')
+            bot.send_photo(chat_id=message.chat.id, photo='https://sun9-34.userapi.com/impg'
+                                                          '/Rbrca4CUrtppbhh8IXQe4lThb5pyHQ8S4wTWrA/4tB7ixGmuQo.jpg'
+                                                          '?size=1218x1624&quality=96&proxy=1&sign'
+                                                          '=fd4101f0b0a76f36a877e16c401b4b24&type=album')
         else:
-            bot.send_message(message.chat.id, "(((нэт)))")
+            bot.send_message(message.chat.id, 'непонял')
+
+            # bot.send_video_note(chat_id=message.chat.id, video_note='')
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -92,7 +118,7 @@ def callback_inline(call):
 
             bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="ОПА",
                                   reply_markup=None)
-
+            # bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
             # bot.answer_callback_qeury(chat_id=call.message.chat.id, show_alert=False, text = "POH")
     except Exception as e:
         print(repr(e))
